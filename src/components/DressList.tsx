846db2eb8fb8f99568090f09eb208d5073cd6201
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-11-08 14:07:00 +0500

    Update DressList.tsx

--- src/components/DressList.tsx
+++ src/components/DressList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import type { DressItemType } from "../types/DressItemType"
 import DressItem from "./DressItem"
 
@@ -5,10 +6,16 @@ interface DressListProps {
   items: DressItemType[];
 }
 
-const DressList = ({ items }: DressListProps) => {
+const DressList = ({items}: DressListProps) => {
+
+    const [initialItem,setInitialItems] = useState<DressItemType[]>([])
+
+    useEffect(()=>{
+        setInitialItems(items);
+    },[items])
   return (
     <div className="flex justify-around w-full">
-      {items.map((item) => (
+      {initialItem.map((item) => (
         <div key={item.id} >
           <DressItem product={item} />
         </div>
